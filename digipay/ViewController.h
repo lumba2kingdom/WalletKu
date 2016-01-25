7fcbaafbb17ec0c8a3474f028344baafdadf0a1e
Lutfi Azhar <lutfi@berrybenka.com>
2016-01-25 10:45:47 +0700

    Initial Commit

---
+++ digipay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  digipay
+//
+//  Created by Lutfi Azhar on 1/25/16.
+//  Copyright © 2016 Lutfi Azhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ digipay/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  digipay
+//
+//  Created by Lutfi Azhar on 1/25/16.
+//  Copyright © 2016 Lutfi Azhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
